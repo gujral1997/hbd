c173da48c1c7ff63c3734bdb6162071541174d4f
Ansh Gujral <anshgujral1997@gmail.com>
2018-08-09 23:23:44 +0530

    firts version

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 import Hbd from './Hbd';
 import Happy from './Happy'
 
 export default class App extends React.Component {
 
+  componentDidMount() {
+    setTimeout(()=>{this.setState({happyBirthday: true})}, 3000);
+    setTimeout(()=>{this.setState({press: true})}, 5000);
+  }
+
   state = {
-    icon: false
+    icon: false,
+    happyBirthday: false,
+    press: false,
+    afterPress: false
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={()=>this.setState({icon:!this.state.icon})}>
+        <TouchableOpacity onPress={()=>{
+          this.setState({
+            icon:!this.state.icon,
+            happyBirthday: !this.state.happyBirthday,
+            press: false,
+            afterPress: !this.state.afterPress
+          })
+      }} style={styles.animation}>
         {this.state.icon?(<Hbd/>):
       (
         <Happy/>
       )}
         </TouchableOpacity>
+        {this.state.happyBirthday?(
+          <Text>Happy Birthday Ullu!!</Text>
+        ):(
+          null
+        )}
+        {
+          this.state.press?(
+            <Text>Press the gift to open it.</Text>
+          ):null
+        }
+        {
+          this.state.afterPress?(
+            <Text>Yeh le China ka Flag</Text>
+          ):null
+        }
       </View>
     );
   }
